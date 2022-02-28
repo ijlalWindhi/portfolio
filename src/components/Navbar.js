541e4718cca8d3366031aea205c26e3237a8f849
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-02-28 22:34:29 +0700

    styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <>
+    <div className='navbar'>
         <Link to="/" className='logo'>DhisaPro</Link>
         <ul>
             <li><Link to="/about" className='navbar-text'>about</Link></li>
             <li><Link to="/project" className='navbar-text'>project</Link></li>
             <li><Link to="/work" className='navbar-text'>work</Link></li>
         </ul>
-    </>
+    </div>
   )
 }
 
