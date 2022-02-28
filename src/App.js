4817c49767d428586a26fbf248e798b9d8d2c867
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-02-28 22:29:54 +0700

    import library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-     <Navbar />
+     <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
     </>
   );
 }
